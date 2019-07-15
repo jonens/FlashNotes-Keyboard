bad9c073d7f93d3085d04cac15a554b257ac5d69
Jon Ensminger <jon.ensminger@att.net>
2019-07-15 11:32:06 -0400

    Updates to score retrieval
    Improved js and php files to allow retrieval of top scores

---
+++ php/game_connect.php
@@ -0,0 +1,16 @@
+﻿<?php
+	//header("Content-type: text/xml");
+	//$db = parse_ini_file('../../../../../php_includes/db.ini')
+	include('../../../../php_includes/config_db.php');
+	//$db = parse_ini_file('db.ini')
+	//$hostname = $db['hostname'];
+	//$username = $db['username'];
+	//$password = $db['password'];
+	//$database = 'pedaplus_flashgames';
+	header("Content-type: text/xml");
+	$link = mysqli_connect($hostname,	$username, $password, $database);
+	if (mysqli_connect_errno()) {
+		die("Game db connection failed: %s\n" + mysqli_connect_error());
+		exit();
+	}
+?>
